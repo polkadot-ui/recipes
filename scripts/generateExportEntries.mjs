bcf2e020bd0ab2a99b0f8e1ae158dbf71f01ab1e
wirednkod <wirednkod@gmail.com>
2023-11-21 01:21:04 +0200

    initial commit

---
+++ scripts/generateExportEntries.mjs
@@ -0,0 +1,61 @@
+// Copyright 2023 @polkadot-cloud/polkadot-cloud authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { existsSync } from "fs";
+import fs from "fs/promises";
+import { format } from "prettier";
+
+// Generates bundle entry files based on the content of the lib/ folder.
+//
+// - `lib/index.tsx` is generated to house all recipes exports.
+export const generateExportEntries = async ({ ignore }) => {
+  // Iterates through a provided directory and returns all component paths.
+  const getDirFolders = async (dir) => {
+    const folders = [];
+
+    // If directory contains index.tsx file, stop.
+    if (dir !== "./lib" && existsSync(`${dir}/index.tsx`)) {
+      folders.push(dir);
+    } else {
+      // for each folder in this directory, loop again.
+      for (let file of await fs.readdir(dir))
+        if ((await fs.stat(dir + "/" + file)).isDirectory())
+          if (!ignore.includes(file))
+            folders.push(...(await getDirFolders(dir + "/" + file)));
+    }
+    return folders;
+  };
+
+  // Iterate through all files and construct entries.
+  const recipes = [];
+  for (let name of await getDirFolders("./lib")) {
+    recipes.push({
+      export: name.split("/").pop(),
+      from: name,
+    });
+  }
+
+  // Construct entry files.
+  await writeFormattedFile(
+    "./lib/index.tsx",
+    generateExportLines(recipes, "./")
+  );
+};
+
+// Generate export lines for an array of items.
+const generateExportLines = (items, basePath) =>
+  items
+    .map(
+      (item) =>
+        `export { ${item.export} } from "${basePath}${item.from
+          .split("/")
+          .slice(2)
+          .join("/")}";`
+    )
+    .join("\n");
+
+// Write formatted TypeScript code to a file.
+const writeFormattedFile = async (path, lines) => {
+  const content = await format(lines, { parser: "typescript" });
+  await fs.writeFile(path, content);
+};
