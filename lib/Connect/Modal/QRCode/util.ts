c756435898cf45f0a434b115fc361ae404a05158
wirednkod <wirednkod@gmail.com>
2024-01-01 12:42:37 +0200

    Replace dates to 2024

--- lib/Connect/Modal/QRCode/util.ts
+++ lib/Connect/Modal/QRCode/util.ts
@@ -1,4 +1,4 @@
-// Copyright 2023 @polkadot-cloud/recipes authors & contributors
+// Copyright 2024 @polkadot-cloud/recipes authors & contributors
 // SPDX-License-Identifier: GPL-3.0-only
 
 import { isString, u8aConcat, u8aToU8a } from "@polkadot/util";
